934552b4c3d3aaa210edcdeb6bfcca8453a0a145
lloyddominic26 <ademic26@yahoo.com>
2023-04-17 03:24:31 +0800

    Create Repository for HOA

---
+++ src/main/java/ccinfom/hoa/repository/HoaRepository.java
@@ -0,0 +1,7 @@
+package ccinfom.hoa.repository;
+
+import ccinfom.hoa.model.Hoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoaRepository extends JpaRepository<Hoa, String> {
+}
