fe2ef699dfa946ea6c7ec9a1bf51b7d6d31088b0
lloyddominic26 <ademic26@yahoo.com>
2023-04-17 03:22:24 +0800

    Added findAllLimit() method

--- src/main/java/ccinfom/hoa/repository/AssetDonationRepository.java
+++ src/main/java/ccinfom/hoa/repository/AssetDonationRepository.java
@@ -2,6 +2,13 @@ package ccinfom.hoa.repository;
 
 import ccinfom.hoa.model.AssetDonation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AssetDonationRepository extends JpaRepository<AssetDonation, Integer> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM asset_donations ORDER BY donation_id LIMIT :limit")
+    List<AssetDonation> findAllLimit(@Param("limit") int limit);
 }
--- src/main/java/ccinfom/hoa/repository/AssetRepository.java
+++ src/main/java/ccinfom/hoa/repository/AssetRepository.java
@@ -2,6 +2,13 @@ package ccinfom.hoa.repository;
 
 import ccinfom.hoa.model.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AssetRepository extends JpaRepository<Asset, Integer> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM assets ORDER BY asset_id LIMIT :limit")
+    List<Asset> findAllLimit(@Param("limit") int limit);
 }
