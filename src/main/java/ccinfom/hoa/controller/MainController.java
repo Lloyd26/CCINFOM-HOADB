8e62b4f307bfe583deeeef870bf7662c38d09597
lloyddominic26 <ademic26@yahoo.com>
2023-04-17 03:28:25 +0800

    Updated Main Controller, Fixed attributes for dashboard

--- src/main/java/ccinfom/hoa/controller/MainController.java
+++ src/main/java/ccinfom/hoa/controller/MainController.java
@@ -1,9 +1,6 @@
 package ccinfom.hoa.controller;
 
-import ccinfom.hoa.repository.HomeownerRepository;
-import ccinfom.hoa.repository.PeopleRepository;
-import ccinfom.hoa.repository.PropertyRepository;
-import ccinfom.hoa.repository.ZipcodeRepository;
+import ccinfom.hoa.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,14 +18,39 @@ public class MainController {
     private PropertyRepository propertyRepository;
     @Autowired
     private ZipcodeRepository zipcodeRepository;
+    @Autowired
+    private OutsiderRepository outsiderRepository;
+    @Autowired
+    private OfficerRepository officerRepository;
+    @Autowired
+    private AssetRepository assetRepository;
+    @Autowired
+    private AssetActivityRepository assetActivityRepository;
+    @Autowired
+    private AssetTransactionRepository assetTransactionRepository;
+    @Autowired
+    private AssetRentalRepository assetRentalRepository;
+    @Autowired
+    private AssetTransferRepository assetTransferRepository;
+    @Autowired
+    private DonorRepository donorRepository;
+    @Autowired
+    private DonatedAssetRepository donatedAssetRepository;
+    @Autowired
+    private DonationPictureRepository donationPictureRepository;
+    @Autowired
+    private AssetDonationRepository assetDonationRepository;
 
     @GetMapping("/")
     public String getMain(Model model) {
+        model.addAttribute("nav_page", "Dashboard");
+        model.addAttribute("page_title", "Dashboard");
         model.addAttribute("homeowners_count", homeownerRepository.count());
         model.addAttribute("people_count", peopleRepository.count());
         model.addAttribute("properties_count", propertyRepository.count());
-        model.addAttribute("recent_people", peopleRepository.getRecentPeople(5));
-        model.addAttribute("zipcodes", zipcodeRepository.findAll());
+        model.addAttribute("officers_count", officerRepository.count());
+        model.addAttribute("recent_assets", assetRepository.findAllLimit(10));
+        model.addAttribute("recent_donations", assetDonationRepository.findAllLimit(10));
         return "index";
     }
 }
